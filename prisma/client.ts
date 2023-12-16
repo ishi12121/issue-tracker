6d2a258a10e875abae240ea1237ba8ae56b7d177
ishi12121 <isht1402@gmail.com>
2023-12-16 12:27:01 +0530

    Build an api for creating issues

---
+++ prisma/client.ts
@@ -0,0 +1,18 @@
+//the code here ensures that we don't have multiple instances running 
+// we are running a single client at a time
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
