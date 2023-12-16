33ccbcd5806fb92c2a6ad737a0c80734c5a0dd64
ishi12121 <isht1402@gmail.com>
2023-12-16 12:27:01 +0530

    Build an api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/prisma/client';
+
+
+export async function POST(request: NextRequest) {
+   
+    const body = await request.json();
+
+
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description },
+    });
+
+    return NextResponse.json(newIssue, { status: 201 });
+}
