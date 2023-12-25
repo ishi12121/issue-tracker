bf54631039e5e8dcb6f8695e537002d1e30c5a51
ishi12121 <isht1402@gmail.com>
2023-12-26 01:58:21 +0530

    styled the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,3 +1,5 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
@@ -18,10 +20,14 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
       <div>
-          <p>{issue.title}</p>
+          <Heading>{issue.title}</Heading>
+          <Flex className='gap-3 ' my='2'>
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString() }</Text>
+          </Flex>
+          <Card>
           <p>{issue.description}</p>
-          <p>{issue.status}</p>
-          <p>{issue.createdAt.toDateString() }</p>
+          </Card>
       </div>
   )
 }
