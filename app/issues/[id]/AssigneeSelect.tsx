cda4c81f51789f214092221a3bd89443829cc9e0
ishi12121 <isht1402@gmail.com>
2024-01-05 19:15:56 +0530

    Refactor AssigneeSelect components

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,36 +1,31 @@
 "use client"
+import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
-import { Select } from "@radix-ui/themes"
+import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import {Skeleton} from "@/app/components";
 import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const { data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get("/api/users").then(res => res.data),
-    staleTime: 60 * 1000, //60s
-    retry: 3
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
- 
+ const assignIssue  = (userId:string) => {
+  axios.patch('/api/issues/' + issue.id, {
+    assignedToUserId: userId === " " ? null : userId
+  }).catch(() => {
+    toast.error("Failed to update the assignee")
+  });
+}
   return (
     <>
     
       <Select.Root
         defaultValue={issue.assignedToUserId || " "}
-        onValueChange={(userId) => {
-          axios.patch('/api/issues/' + issue.id, {
-            assignedToUserId: userId === " " ? null : userId
-          }).catch(() => {
-            toast.error("Failed to update the assignee")
-          });
-        }}>
+        onValueChange={assignIssue}>
         <Select.Trigger />
         <Select.Content >
           <Select.Group>
@@ -53,5 +48,11 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+const useUsers = () =>  useQuery<User[]>({
+  queryKey: ['users'],
+  queryFn: () => axios.get("/api/users").then(res => res.data),
+  staleTime: 600 * 1000, //60s
+  retry: 3
+});
 
 export default AssigneeSelect;
