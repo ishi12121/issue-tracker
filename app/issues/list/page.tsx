f48a2e66d78b062c5944f6229fcebc42cfbce422
ishi12121 <isht1402@gmail.com>
2024-01-05 22:48:33 +0530

    Paginate Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -6,9 +6,14 @@ import NextLink from "next/link";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "@/app/components/Pagination";
 
 interface Props {
-  searchParams: {status :Status, orderBy: keyof Issue}
+  searchParams: {
+    status: Status,
+    orderBy: keyof Issue
+    page: string
+  };
 }
 const IssuesPage = async ({
   searchParams,
@@ -34,23 +39,26 @@ const IssuesPage = async ({
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
-  
+  const where = { status };
+    
   const orderBy = columns
     .map(column => column.value)
     .includes(searchParams.orderBy) 
     ? { [searchParams.orderBy]: 'asc' }
     : undefined;   
+  
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
   const issues = await prisma.issue.findMany({
-    where: {
-      status
-    },
-    orderBy
+    where,
+    orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize
   });
-
+ const issueCount = await  prisma.issue.count({ where})
   return (
     <div>
       <IssueActions />
-
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -64,8 +72,6 @@ const IssuesPage = async ({
               </NextLink>
               {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
             </Table.ColumnHeaderCell> )}
-           
-           
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -73,12 +79,10 @@ const IssuesPage = async ({
             <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.Cell>
-
               <Table.Cell className="hidden md:table-cell">
                 <IssueStatusBadge status={issue.status} />
               </Table.Cell>
@@ -89,6 +93,11 @@ const IssuesPage = async ({
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemCount={issueCount}
+      />
     </div>
   );
 };
