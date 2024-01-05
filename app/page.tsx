e2f89a11bc3e36ecf73507bd1e1d6b470c30f66a
ishi12121 <isht1402@gmail.com>
2024-01-05 22:20:46 +0530

    Build the layout for pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-    <div>DashBoard</div>
+    <>
+       <div>DashBoard</div>
+      <Pagination itemCount={100} pageSize={10} currentPage={4} />
+     
+   </>
+   
   )
 }
