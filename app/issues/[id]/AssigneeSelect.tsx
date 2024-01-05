be98b69b31fc36e9a2c2a816fdd59b7f1c6824cc
ishi12121 <isht1402@gmail.com>
2024-01-05 18:47:18 +0530

    Assign an issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,12 @@
 "use client"
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes"
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import {Skeleton} from "@/app/components";
 
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({issue}: {issue:Issue}) => {
   const { data: users, error, isLoading } = useQuery<User[]>({
     queryKey: ['users'],
     queryFn: () => axios.get("/api/users").then(res => res.data),
@@ -20,16 +20,25 @@ const AssigneeSelect = () => {
 
  
   return (
-    <Select.Root >
-          <Select.Trigger placeholder="Assignee..." />
+    <Select.Root
+      defaultValue={issue.assignedToUserId || " "}
+      onValueChange={(userId) => {
+      axios.patch('/api/issues/' + issue.id, {
+        assignedToUserId: userId === " " ? null : userId
+      })
+    }}>
+          <Select.Trigger/>
           <Select.Content >
               <Select.Group>
               <Select.Label>
                    Suggestions
-                  </Select.Label>
+          </Select.Label>
+          <Select.Item value= " ">
+           Unassigned
+          </Select.Item>
           {users?.map(user =>
             <Select.Item
-              key={user.id} value={user.id}>
+              key={user.id} value={String(user.id)}>
               {user.name}
             </Select.Item>
           )}
