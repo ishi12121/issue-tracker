0971338740081179ab6bb0ede72221df8d3bfa00
ishi12121 <isht1402@gmail.com>
2024-11-07 16:11:53 +0530

    some protection for routes

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -36,28 +36,32 @@ const NavBar = () => {
 
 const NavLinks = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues/list" },
-    { label: "Kanban board", href: "/kanban" },
+    { label: "Tracker", href: "/kanban", visible: status === "authenticated" },
   ];
 
   return (
     <ul className="flex space-x-6">
-      {links.map((link) => (
-        <li key={link.href}>
-          <Link
-            className={classnames({
-              "nav-link": true,
-              "!text-zinc-900": link.href === currentPath,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
-        </li>
-      ))}
+      {links.map(
+        (link) =>
+          link.visible !== false && (
+            <li key={link.href}>
+              <Link
+                className={classnames({
+                  "nav-link": true,
+                  "!text-zinc-900": link.href === currentPath,
+                })}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          )
+      )}
     </ul>
   );
 };
